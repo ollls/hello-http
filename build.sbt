bde6b7df4aeeb47ab82fbc218d20012422d2dd27
ostrygun <oleg_strygun@intuit.com>
2023-02-07 16:27:48 -0800

    version2 and scala3

--- build.sbt
+++ build.sbt
@@ -1,21 +1,29 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.4"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
+ThisBuild / scalaVersion := "3.2.1"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.example"
 ThisBuild / organizationName := "example"
+
+resolvers += "Staging" at "https://s01.oss.sonatype.org/content/repositories/iogithubollls-1038"
+
+Runtime / unmanagedClasspath += baseDirectory.value / "src" / "main" / "resources"
 
 lazy val root = (project in file("."))
   .settings(
     name := "hello-http",
-    libraryDependencies ++= Seq( 
-       "io.github.ollls"  %% "zio-tls-http" % "1.2-m3"
+    libraryDependencies ++= Seq(
+      "io.github.ollls" %% "zio-tls-http" % "2.0",
+      "dev.zio" %% "zio-logging-slf4j" % "2.1.5",
+      "org.slf4j" % "slf4j-api" % "2.0.4",
+      "ch.qos.logback" % "logback-classic" % "1.3.5"
     )
-   )
+  )
 
-   scalacOptions ++= Seq(
-  "-Xfatal-warnings",
+scalacOptions ++= Seq(
   "-deprecation",
+  "-feature",
+  "-no-indent"
 )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,26 +1,24 @@
 package example
 
-import zio.App
-import zio.ZEnv
 import zio.ZIO
+import zio.ZLayer
 import zio.json._
 
-import zhttp.TcpServer
+import zhttp.{TcpServer, TLSServer}
 import zhttp.HttpRouter
-import zhttp.MyLogging.MyLogging
-import zhttp.MyLogging
-import zhttp.LogLevel
 import zhttp.HttpRoutes
 import zhttp.dsl._
 import zhttp.Response
 import zhttp.Method._
-
+import zio.logging.backend.SLF4J
 
 object UserRecord {
-  implicit val decoder: JsonDecoder[UserRecord] = DeriveJsonDecoder.gen[UserRecord]
-  implicit val encoder: JsonEncoder[UserRecord] = DeriveJsonEncoder.gen[UserRecord]
+  implicit val decoder: JsonDecoder[UserRecord] =
+    DeriveJsonDecoder.gen[UserRecord]
+  implicit val encoder: JsonEncoder[UserRecord] =
+    DeriveJsonEncoder.gen[UserRecord]
 }
-case class UserRecord(val uid: String )
+case class UserRecord(val uid: String)
 
 //Please see URL, for more examples/use cases.
 //https://github.com/ollls/zio-tls-http/blob/dev/examples/start/src/main/scala/MyServer.scala
@@ -29,49 +27,43 @@ case class UserRecord(val uid: String )
    How to switch to secure channel
 
    import zhttp.TLSServer
-   val myHttp = new TLSServer[MyEnv]( port = 8443, 
-                                      keepAlive = 4000, 
-                                      serverIP = "0.0.0.0", 
-                                      keystore = "keystore.jks", "password", 
+   val myHttp = new TLSServer[MyEnv]( port = 8443,
+                                      keepAlive = 4000,
+                                      serverIP = "0.0.0.0",
+                                      keystore = "keystore.jks", "password",
                                       tlsVersion = "TLSv1.2" )
-*/                                      
+ */
 
-object ServerExample extends zio.App {
+object ServerExample extends zio.ZIOAppDefault {
 
-  object param1 extends QueryParam( "param1")
+  override val bootstrap = zio.Runtime.removeDefaultLoggers ++ SLF4J.slf4j ++ zio.Runtime.enableWorkStealing
 
-  def run(args: List[String]) = {
+  object param1 extends QueryParam("param1")
 
+  def run = {
 
-    val r =  HttpRoutes.of {
-       case GET -> Root / "health" =>
-        ZIO(Response.Ok().asTextBody("Health Check Ok"))
+    val r = HttpRoutes.of {
+      case GET -> Root / "health" =>
+        ZIO.attempt(Response.Ok().asTextBody("Health Check Ok"))
 
-       case GET -> Root / "user" :? param1( par ) => ZIO( Response.Ok().asTextBody( "param1=" + par ))
+      case GET -> Root / "user" :? param1(par) =>
+        ZIO.attempt(Response.Ok().asTextBody("param1=" + par))
 
-       case req @ POST -> Root / "test" =>
-         for {
-           rec <- req.fromJSON[UserRecord]
-           _   <- MyLogging.info("my_application", "UID received: " + rec.uid )
-         } yield( Response.Ok().asTextBody( "OK " + rec.uid ) )
+      //Expected POST body: { "uid" :  "username" }  
+      case req @ POST -> Root / "test" =>
+        for {
+          rec <- req.fromJSON[UserRecord]
+          _ <- ZIO.log("UID received: " + rec.uid)
+        } yield (Response.Ok().asTextBody("OK " + rec.uid))
     }
 
+    val myHttpServer =
+      new TcpServer[Any](port = 8080, keepAlive = 2000, serverIP = "0.0.0.0")
+    val myHttpRouter = new HttpRouter[Any](r)
 
+    // val AttributeLayer = ZLayer.fromZIO(ZIO.succeed("flag#1-1"))
 
-    type MyEnv = MyLogging
-
-    val myHttp = new TcpServer[MyEnv]( port= 8080, keepAlive = 2000, serverIP = "0.0.0.0")
-
-    val myHttpRouter = new HttpRouter[MyEnv]( r )
-    
-
-    val logger_L = MyLogging.make( maxLogSize = 1024*1024, maxLogFiles = 7,
-                                   ("console" -> LogLevel.Trace), 
-                                   ("access" -> LogLevel.Info), 
-                                   ( "my_application" -> LogLevel.Info) )
-
-    myHttp.run( myHttpRouter.route ).provideSomeLayer[ZEnv](logger_L).exitCode
-
+    myHttpServer.run(r) // .provideSomeLayer(AttributeLayer)
 
   }
 }
