d92724d0ef16a988a8cb71ba65a7b4594cf2801c
ostrygun <oleg_strygun@intuit.com>
2023-02-07 18:00:37 -0800

    update

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -23,16 +23,6 @@ case class UserRecord(val uid: String)
 //Please see URL, for more examples/use cases.
 //https://github.com/ollls/zio-tls-http/blob/dev/examples/start/src/main/scala/MyServer.scala
 
-/*
-   How to switch to secure channel
-
-   import zhttp.TLSServer
-   val myHttp = new TLSServer[MyEnv]( port = 8443,
-                                      keepAlive = 4000,
-                                      serverIP = "0.0.0.0",
-                                      keystore = "keystore.jks", "password",
-                                      tlsVersion = "TLSv1.2" )
- */
 
 object ServerExample extends zio.ZIOAppDefault {
 
